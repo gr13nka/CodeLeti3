56813f6c4fbdacd70c01e40c3f07260b56b18770
Andrey <34208591+gr13nka@users.noreply.github.com>
2023-12-20 18:47:35 +0300

    Update event.cpp

--- oop/CW_OOP/CW_OOP/event.cpp
+++ oop/CW_OOP/CW_OOP/event.cpp
@@ -16,6 +16,25 @@ std::string Event::GetName()
     return Name;
 }
 
+int Event::GetStart()
+{
+    return Start;
+}
+
+int Event::GetEnd()
+{
+    return End;
+}
+
+void Event::SetStart(int newStart)
+{
+    Start = newStart;
+}
+
+void Event::SetEnd(int newEnd)
+{
+    End = newEnd;
+}
 int Event::GetType()
 {
     return Type;

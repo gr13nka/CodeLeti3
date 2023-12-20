acd635e0fc26701f02397d45ab8ac7988417c278
Andrey <34208591+gr13nka@users.noreply.github.com>
2023-12-20 18:42:03 +0300

    Update eventgenerator.cpp

--- oop/CW_OOP/CW_OOP/eventgenerator.cpp
+++ oop/CW_OOP/CW_OOP/eventgenerator.cpp
@@ -1,7 +1,12 @@
 #include "eventgenerator.h"
+#include <QCoreApplication>
+#include <QTimer>
+#include <QDebug>
+#include <vector>
 
 EventGenerator::EventGenerator()
 {
-    //long int timer = time(NULL);
-    //printf("%ld", timer);
+    генерируем 20 пар чисел между ними разница 1-3 сами числа от 1 до 10
+
+    записываем эти числа в цикле в поля ивентов
 }
